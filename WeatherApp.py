e7f98e32de769ba1035aa8387b75f0663d987622
emmettk12 <48069872+emmettk12@users.noreply.github.com>
2021-03-22 18:39:30 -0400

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import requests
 
+
+import tkinter as tk
+import requests
+
 HEIGHT = 500
 WIDTH = 600
 
@@ -28,16 +32,32 @@ def get_weather(city):
 
 	label['text'] = format_response(weather)
 
-
+def tempForDrink(temp):
+    if (temp<=40):
+        drink="Get a hot drink"
+       # return(drink)
+    elif (temp>=41) and (temp<=60):
+        drink="Get a  warm team"
+        #print(drink)
+    elif (temp>=61) and (temp<=80):
+        drink="Get a  ice coffee"
+        #print(drink)
+    elif (temp>=81) and (temp<=100):
+        drink="Get a  iced sweet tea"
+        #print(drink)
+    else:
+        drink="Get ice cream"
+        #print(drink)
+    label['text'] = drink
 
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-background_image = tk.PhotoImage(file='landscape.png')
-background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
+#background_image = tk.PhotoImage(file='landscape.png')
+#background_label = tk.Label(root, image=background_image)
+#background_label.place(relwidth=1, relheight=1)
 
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
@@ -48,6 +68,9 @@ entry.place(relwidth=0.65, relheight=1)
 button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
+button1 = tk.Button(frame, text="Drink", font=40, command=lambda: tempForDrink(40))
+button1.place(relx=0.7, relheight=1, relwidth=0.3)
+
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
@@ -55,3 +78,5 @@ label = tk.Label(lower_frame)
 label.place(relwidth=1, relheight=1)
 
 root.mainloop()
+
+
